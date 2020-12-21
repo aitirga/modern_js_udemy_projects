348376e993ee6b9ef42ead1b5d1b4dea6a7238ff
aitirga <aitirga@gmail.com>
2020-12-21 19:01:26 +0100

    feat: course exercise 1

---
+++ course/js_sandbox/app.js
@@ -0,0 +1,91 @@
+// var name = 'John Doe';
+// console.log(name);
+//
+// name = "Steve Smith";
+// console.log(name);
+//
+// var greeting;
+// console.log(greeting)
+// console.log("hye")
+//
+// var $name = 'John'
+// var firstName = "test"
+
+// LET
+
+// let name = "John Doe"
+// console.log(name)
+//
+// name = "Steve Smith"
+// console.log(name)
+//
+// const name = 'John'
+// console.log(name);
+// name = "Sarah"
+//
+// const person = {
+//     name: 'John',
+//     age: 30
+// }
+//
+// person.name = 'Sara';
+//
+// console.log(person)
+
+// const numbers = [1, 2, 3]
+// numbers.push(2)
+// console.log(numbers)
+
+// Primitive
+
+// // String
+// const name = 'John Dow'
+//
+// // Number
+// const age = 45
+//
+// const car = null
+//
+// console.log(typeof car)
+
+// const today = new Date();
+// console.log(today)
+// console.log(typeof today)
+
+let val;
+
+// Number to string
+
+val = String(555);
+val = String(4+4);
+
+// Bool to string
+val = String(true);
+
+// Date to string
+val = String(new Date())
+
+// Array to string
+val = String([1, 2, 3])
+
+// toString()
+
+val = (5).toString()
+val = (true).toString()
+
+// String to number
+val = Number('5');
+val = Number(true)
+val = Number(null)
+val = Number('hello')
+
+// Additional method
+
+
+//Output
+
+
+console.log(val)
+console.log(typeof val)
+// console.log(val.length)
+console.log(val.toFixed(2))
